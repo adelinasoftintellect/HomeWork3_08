3320cdda74117e3eee6ceeeed41ea58024b97715
Adi <adelina_yordanova11@abv.bg>
2016-05-16 06:58:41 +0300

    Homework3_09.

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in, "UTF-8");
+
+		System.out.println("Please enter integer a:");
+		int a = input.nextInt();
+
+		System.out.println("Please enter integer b:");
+		int b = input.nextInt();
+
+		System.out.println("Please enter integer c:");
+		int c = input.nextInt();
+		double d = -(b * b) - (4 * a * c);
+		double x = -b / (2 * a);
+		double SqRt = Math.sqrt(d);
+		double x1 = ((-b + SqRt) / 2 * a);
+		double x2 = ((-b - SqRt) / 2 * a);
+
+		if (a == 0) {
+			System.out.println("The equation is not square");
+
+		} else {
+
+			System.out.println("d = - (b*b) - (4*a*c) = " + d);
+			if (d < 0) {
+				System.out.println("The equation has no real roots");
+
+			} else if (d == 0) {
+				System.out.println("x = -b/2a = " + x);
+
+			} else {
+				System.out.println("x = -b/2a = " + x1);
+				System.out.println("x = -b/2a = " + x2);
+			}
+		}
+		input.close();
+
+	}
+}
